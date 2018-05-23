0d628dfef384d1639bc6ee5b0ebd59aa9b013c8f
AlexJackman <jackmanalex228@gmail.com>
2018-05-23 15:10:41 -0600

    edit some code in main, set up proposed skeliton for project

--- Milestone_01/Milestone_01/main.cpp
+++ Milestone_01/Milestone_01/main.cpp
@@ -1,30 +1,30 @@
 #include <iostream>
 using namespace std;
 
-/*	I/O:
-		READ		10	read into memory
-		Write		11
+/*	I/O
+		READ			10	read into memory
+		Write			11
 	Load/store operations
-		LOAD		20	load from memory location into accumulator
-		STORE		21
+		LOAD			20	load from memory location into accumulator
+		STORE			21
 	Arithmetic operation
-		ADD			30	number in memory location + number in accumulator (keep result in accumulator)
-		SUB			31
-		DIV			32
-		MULT		33
+		ADD				30	number in memory location + number in accumulator (keep result in accumulator)
+		SUB				31
+		DIV				32
+		MULT			33
 	Control operations
-		BRANCH		40	branch into specific location in memory
-		BRANCHNEG	41
-		BRANCHZERO	42
-		HALT		43
+		BRANCH			40	branch into specific location in memory
+		BRANCHNEG		41
+		BRANCHZERO		42
+		HALT			43
 	Debugging opreations
 		MEMDUMP			print the memory image on the screen
 		BREAK
 		CONTINUE
-	Terminate		-99999
+	Terminate program	-99999
 */
 
-void Parse_Input_From_File() {
+void Parse_Input_From_File(int command, int location) {
 
 }
 
@@ -33,17 +33,21 @@ void Calculate_Operations() {
 	const int MEMORY_SIZE = 99;
 	int memory[MEMORY_SIZE];
 	int accumulator;
-	int valueFromInput;
 	int result;
 
+	int command, location;
+
+	// Parse_Input_From_File(command, location)
+
+	// Things will be calculated here
+
+	// Output_Data()
 	
 }
 
-void Output_Data() {
-
-}
+// Output_Data()
 
 int main() {
-	cout << "Hello World!" << endl;
+	Calculate_Operations();
 	return 0;
 }
