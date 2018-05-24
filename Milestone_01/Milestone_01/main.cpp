846095d8175feb1ba3ff005439850d28cb9d34d4
alanchavezt <alanchavez1@gmail.com>
2018-05-24 08:46:11 -0600

    Update main.cpp

--- Milestone_01/Milestone_01/main.cpp
+++ Milestone_01/Milestone_01/main.cpp
@@ -113,7 +113,107 @@ void Calculate_Operations() {
 
 // Output_Data()
 
-int main() {
-	Calculate_Operations();
-	return 0;
+class Instruction{
+    int code;
+    string name;
+public:
+    Instruction(int c, string n) : code(c), name(n){};
+    Instruction(const Instruction &i) : code(i.code), name(i.name) { }
+    
+    void setCode(int c){
+        this->code = c;
+    }
+    
+    void setName(string n){
+        this->name = n;
+    }
+    
+    int getCode(){
+        return code;
+    }
+    
+    string getName(){
+        return name;
+    }
+};
+
+
+int main(int argc, const char * argv[]) 
+{
+    Calculate_Operations();
+	
+    vector<Instruction> *vecptr = new vector<Instruction>;
+    vecptr->push_back(Instruction(10,"READ"));
+    vecptr->push_back(Instruction(11,"WRITE"));
+    vecptr->push_back(Instruction(20,"LOAD"));
+    vecptr->push_back(Instruction(21,"STORE"));
+    vecptr->push_back(Instruction(30,"ADD"));
+    vecptr->push_back(Instruction(31,"SUB"));
+    vecptr->push_back(Instruction(32,"DIV"));
+    vecptr->push_back(Instruction(33,"MULT"));
+    vecptr->push_back(Instruction(40,"BRANCH"));
+    vecptr->push_back(Instruction(41,"BRANCHNEG"));
+    vecptr->push_back(Instruction(42,"BRANCHZERO"));
+    vecptr->push_back(Instruction(43,"HALT"));
+    vecptr->push_back(Instruction(-99999,"Terminate"));
+
+   
+    unsigned long response = 0;
+    bool menuQuit = false;
+    
+    while (menuQuit == false)
+    {
+        do
+        {
+            std::cout << "What action would you like to take?\n"<<endl;
+            for (vector<Instruction>::iterator it = vecptr->begin() ; it != vecptr->end(); ++it){
+                cout<<it->getCode()<< " - " << it->getName()<<endl;
+            }
+            cin >> response;
+            cout << "\n";
+        }
+        while (response == -99999);
+        
+        switch (response)
+        {
+            case 10:
+                std::cout << "Seasons of the Year was selected!\n\n";
+                break;
+                
+            case 11:
+                std::cout << "BINGO was selected!\n\n";
+                break;
+                
+            case 20:
+                std::cout << "(My Loop) was selected!\n\n";
+                break;
+                
+            case 21:
+                std::cout << "(My Loop) was selected!\n\n";
+                break;
+                
+            case 30:
+                std::cout << "(My Loop) was selected!\n\n";
+                break;
+            
+            case 31:
+                std::cout << "(My Loop) was selected!\n\n";
+                break;
+                
+            case 32:
+                std::cout << "(My Loop) was selected!\n\n";
+                break;
+                
+            case 4:
+                std::cout << "Exiting....\n";
+                menuQuit = true;
+                break;
+                
+            default:
+                std::cout << "\n** Invalid Menu Selection!! **\n\n";
+                break;
+        }
+    }
+    
+    return 0;
 }
